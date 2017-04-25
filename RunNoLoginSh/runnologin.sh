d04d9999a5d58d8026f6b0e5277d4c1faac86467
heguangyu <332512308@qq.com>
2017-04-25 10:06:17 +0800

    ^_^ a spell fix

--- RunNoLoginSh/runnologin.sh
+++ RunNoLoginSh/runnologin.sh
@@ -12,7 +12,7 @@ if [ $(id -u) != "0" ]; then
     exit 1
 fi
 
-www=`ps aux|grep php-fpm|grep -v root|grep -v baocai|awk 'NR==1 {print $1}'`
+www=`ps aux|grep php-fpm|grep -v "root"|grep -v "grep"|awk 'NR==1 {print $1}'`
 echo $www
 
 /bin/su $www -s artisan list 
